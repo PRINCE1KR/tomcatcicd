79f60232c4131d043c874bdc7d3e530adecf20e7
PRINCE1KR <118903165+PRINCE1KR@users.noreply.github.com>
2023-03-11 18:30:18 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "Hello World!";
+        return "Hello Prince its a fantastic to do cicd";
     }
 
 }
